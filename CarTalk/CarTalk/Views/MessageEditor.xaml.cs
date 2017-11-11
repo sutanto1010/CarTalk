058fbf2e1199203b79a71d5dd63f504ae37295d9
Sutanto <sutanto.1010@gmail.com>
2017-11-12 01:02:06 +0700

    Added message editor

---
+++ CarTalk/CarTalk/Views/MessageEditor.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms.Xaml;
+
+namespace CarTalk.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MessageEditor
+	{
+	    private static MessageEditor _instance;
+
+	    private MessageEditor ()
+		{
+			InitializeComponent ();
+		}
+
+	    public static MessageEditor Current => _instance ?? (_instance = new MessageEditor());
+
+	    public void Show()
+	    {
+	        PopupNavigation.PushAsync(this);
+	    }
+
+        public void Hide()
+        {
+            PopupNavigation.PopAsync(true);
+        }
+
+	    private void OnCancelButtonTapped(object sender, EventArgs e)
+	    {
+	        Hide();
+	    }
+	}
+}
